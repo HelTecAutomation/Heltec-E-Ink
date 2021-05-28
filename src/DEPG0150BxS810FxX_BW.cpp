6093b0255a4d69efb6a54620280998b08ceba688
yanxiaolzay <1977735808@qq.com>
2021-05-28 12:17:49 +0800

    refactor: refactoring 1.54 program

--- src/DEPG0150BxS810FxX_BW.cpp
+++ src/DEPG0150BxS810FxX_BW.cpp
@@ -1,75 +1,352 @@
 #include "DEPG0150BxS810FxX_BW.h"
 
-/******************************* init ***************************************************/
-
-void DEPG0150BxS810FxX_BW::EPD_Init(void) {
-/* this calls the peripheral hardware interface, see epdif */
+// High Direction
+int DEPG0150BxS810FxX_BW::HDirInit(void)
+{
 #if defined( ESP32 )
 	SPI.begin(this->clk_pin, MISO,MOSI, this->cs_pin);
 #elif defined( ESP8266 )
 	SPI.pins(this->clk_pin, MISO, MOSI, this->cs_pin);
 	SPI.begin();
 #elif defined( CubeCell_Board )//AB01
-    SPI.begin(this->cs_pin, this->freq, this->spi_num);
+  SPI.begin(this->cs_pin, this->freq, this->spi_num);
 #endif
+	/* this calls the peripheral hardware interface, see epdif */
+	if (!this->spiFlag && IfInit() != 0) {
+		return -1;
+	}
+	this->spiFlag = 1;
+	/* EPD hardware init start */
+	Reset();
 
-  if (IfInit() != 0) {
-    Serial.print("e-Paper init failed");
-    return;
-  }
-  Reset();
-  WaitUntilIdle();//读busy信号
-  SendCommand(0x12); // 软件复位    soft  reset
-  WaitUntilIdle();
-  
-  Serial.println("e-Paper init OK!");	
+	WaitUntilIdle();
+	SendCommand(0x12);  //SWRESET
+	WaitUntilIdle();
+
+	SendCommand(0x01); //Driver output control
+	SendData(0xC7);
+	SendData(0x00);
+	SendData(0x01);
+
+	SendCommand(0x11); //data entry mode
+	SendData(0x01);
+
+	SendCommand(0x44); //set Ram-X address start/end position
+	SendData(0x00);
+	SendData(0x18);    //0x0C-->(18+1)*8=200
+
+	SendCommand(0x45); //set Ram-Y address start/end position
+	SendData(0xC7);   //0xC7-->(199+1)=200
+	SendData(0x00);
+	SendData(0x00);
+	SendData(0x00);
+
+	SendCommand(0x3C); //BorderWavefrom
+	SendData(0x01);
+
+	SendCommand(0x18);
+	SendData(0x80);
+
+	SendCommand(0x22); // //Load Temperature and waveform setting.
+	SendData(0XB1);
+	SendCommand(0x20);
+
+	SendCommand(0x4E);   // set RAM x address count to 0;
+	SendData(0x00);
+	SendCommand(0x4F);   // set RAM y address count to 0X199;
+	SendData(0xC7);
+	SendData(0x00);
+	WaitUntilIdle();
+	/* EPD hardware init end */
+
+	return 0;
+}
+
+// Low Direction
+int DEPG0150BxS810FxX_BW::LDirInit(void)
+{
+#if defined( ESP32 )
+	SPI.begin(this->clk_pin, MISO,MOSI, this->cs_pin);
+#elif defined( ESP8266 )
+	SPI.pins(this->clk_pin, MISO, MOSI, this->cs_pin);
+	SPI.begin();
+#elif defined( CubeCell_Board )//AB01
+  SPI.begin(this->cs_pin, this->freq, this->spi_num);
+#endif
+	/* this calls the peripheral hardware interface, see epdif */
+	if (!this->spiFlag && IfInit() != 0) {
+		return -1;
+	}
+	this->spiFlag = 1;
+	/* EPD hardware init start */
+	Reset();
+
+	WaitUntilIdle();
+	SendCommand(0x12);  //SWRESET
+	WaitUntilIdle();
+
+	SendCommand(0x01); //Driver output control
+	SendData(0xC7);
+	SendData(0x00);
+	SendData(0x00);
+
+	SendCommand(0x11); //data entry mode
+	SendData(0x03);
+
+  SendCommand(0x44);
+  /* x point must be the multiple of 8 or the last 3 bits will be ignored */
+  SendData((0 >> 3) & 0xFF);
+  SendData((200 >> 3) & 0xFF);
+  SendCommand(0x45);
+  SendData(0 & 0xFF);
+  SendData((0 >> 8) & 0xFF);
+  SendData(200 & 0xFF);
+  SendData((200 >> 8) & 0xFF);
+
+	SendCommand(0x3C); //BorderWavefrom
+	SendData(0x01);
+
+	SendCommand(0x18);
+	SendData(0x80);
+
+	SendCommand(0x22); // //Load Temperature and waveform setting.
+	SendData(0XB1);
+	SendCommand(0x20);
+
+	SendCommand(0x4E);   // set RAM x address count to 0;
+	SendData(0x00);
+	SendCommand(0x4F);   // set RAM y address count to 0X199;
+	SendData(0xC7);
+	SendData(0x00);
+	WaitUntilIdle();
+	/* EPD hardware init end */
+
+	return 0;
+}
+
+void DEPG0150BxS810FxX_BW::Clear(void)
+{
+	int w, h;
+	w = (EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1);
+	h = EPD_HEIGHT;
+ 
+	SendCommand(0x24);
+	for (int j = 0; j < h; j++) {
+		for (int i = 0; i < w; i++) {
+			SendData(0xff);
+		}
+	}
+	//DISPLAY REFRESH
+	DisplayFrame();
+}
+
+void DEPG0150BxS810FxX_BW::Display(const unsigned char* frame_buffer)
+{
+	int w = (EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1);
+	int h = EPD_HEIGHT;
+
+	if (frame_buffer != NULL) {
+		SendCommand(0x24);
+		for (int j = 0; j < h; j++) {
+			for (int i = 0; i < w; i++) {
+				SendData(pgm_read_byte(&frame_buffer[i + j * w]));
+			}
+		}
+	}
+
+	//DISPLAY REFRESH
+	DisplayFrame();
+}
+
+void DEPG0150BxS810FxX_BW::DisplayPartBaseImage(const unsigned char* frame_buffer)
+{
+	int w = (EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1);
+	int h = EPD_HEIGHT;
+
+	if (frame_buffer != NULL) {
+		SendCommand(0x24);
+		for (int j = 0; j < h; j++) {
+			for (int i = 0; i < w; i++) {
+				SendData(pgm_read_byte(&frame_buffer[i + j * w]));
+			}
+		}
+
+		SendCommand(0x26);
+		for (int j = 0; j < h; j++) {
+			for (int i = 0; i < w; i++) {
+				SendData(pgm_read_byte(&frame_buffer[i + j * w]));
+			}
+		}
+	}
+
+	//DISPLAY REFRESH
+	DisplayFrame();
+}
+
+void DEPG0150BxS810FxX_BW::DisplayPartBaseWhiteImage(void)
+{
+	int w = (EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1);
+	int h = EPD_HEIGHT;
+
+	SendCommand(0x24);
+	for (int j = 0; j < h; j++) {
+		for (int i = 0; i < w; i++) {
+			SendData(0xff);
+		}
+	}
+
+	SendCommand(0x26);
+	for (int j = 0; j < h; j++) {
+		for (int i = 0; i < w; i++) {
+			SendData(0xff);
+		}
+	}
+
+
+	//DISPLAY REFRESH
+	DisplayFrame();
 }
 
 
-/********************************* update ************************************************/
-void DEPG0150BxS810FxX_BW::EPD_Update(void) {     			
-  SendCommand(0x20);
-  WaitUntilIdle();
+void DEPG0150BxS810FxX_BW::DisplayPart(const unsigned char* frame_buffer)
+{
+	int w = (EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1);
+	int h = EPD_HEIGHT;
+
+	if (frame_buffer != NULL) {
+		SendCommand(0x24);
+		for (int j = 0; j < h; j++) {
+			for (int i = 0; i < w; i++) {
+				SendData(pgm_read_byte(&frame_buffer[i + j * w]));
+			}
+		}
+	}
+
+	//DISPLAY REFRESH
+	DisplayPartFrame();
 }
 
-/*********************************** deep sleep ******************************************/
-void DEPG0150BxS810FxX_BW::EPD_DeepSleep(void) {
-  SendCommand(0x10);
-  SendData(0x01);   
-  DelayMs(100); 
+
+/**
+ *  @brief: private function to specify the memory area for data R/W
+ */
+void DEPG0150BxS810FxX_BW::SetMemoryArea(int x_start, int y_start, int x_end, int y_end)
+{
+	SendCommand(0x44);
+	/* x point must be the multiple of 8 or the last 3 bits will be ignored */
+	SendData((x_start >> 3) & 0xFF);
+	SendData((x_end >> 3) & 0xFF);
+	SendCommand(0x45);
+	SendData(y_start & 0xFF);
+	SendData((y_start >> 8) & 0xFF);
+	SendData(y_end & 0xFF);
+	SendData((y_end >> 8) & 0xFF);
 }
 
-/******************************** EPD_ALL_image ******************************************/
-void DEPG0150BxS810FxX_BW::EPD_ALL_image(const unsigned char *datas) {
-
-  SendCommand(0x24);   //write RAM for black(0)/white (1)
-
-  for (int i = 0; i < ALLSCREEN_BYTES; i++) {
-    SendData(pgm_read_byte(&datas[i]));
-  }	
-
-  EPD_Update();
+/**
+ *  @brief: private function to specify the start point for data R/W
+ */
+void DEPG0150BxS810FxX_BW::SetMemoryPointer(int x, int y)
+{
+	SendCommand(0x4e);
+	/* x point must be the multiple of 8 or the last 3 bits will be ignored */
+	SendData((x >> 3) & 0xFF);
+	SendCommand(0x4F);
+	SendData(y & 0xFF);
+	SendData((y >> 8) & 0xFF);
+	WaitUntilIdle();
 }
 
-/********************************** All black *********************************************/
-void DEPG0150BxS810FxX_BW::EPD_WhiteScreen_Black(void) {
-  EPD_Load_Data(0x00);
+
+/**
+ *  @brief: update the display
+ *          there are 2 memory areas embedded in the e-paper display
+ *          but once this function is called,
+ *          the the next action of SetFrameMemory or ClearFrame will
+ *          set the other memory area.
+ */
+void DEPG0150BxS810FxX_BW::DisplayFrame(void)
+{
+	//DISPLAY REFRESH
+	SendCommand(0x22);
+	SendData(0xF7);
+	SendCommand(0x20);
+	WaitUntilIdle();
 }
 
-/********************************** All white *********************************************/
-void DEPG0150BxS810FxX_BW::EPD_WhiteScreen_White(void) {
-  EPD_Load_Data(0xff);
+void DEPG0150BxS810FxX_BW::DisplayPartFrame(void)
+{
+	SendCommand(0x22);
+	SendData(0xFF);
+	SendCommand(0x20);
+	WaitUntilIdle();
 }
 
-/********************************** Load Data *********************************************/
-void DEPG0150BxS810FxX_BW::EPD_Load_Data(unsigned char data) {
-  SendCommand(0x24);   //write RAM for black(0)/white (1)
 
-  for (int i = 0; i < ALLSCREEN_BYTES; i++) {
-    SendData(data);
-  }	
+void DEPG0150BxS810FxX_BW::SetFrameMemory(
+        const unsigned char* image_buffer,
+        int x,
+        int y,
+        int image_width,
+        int image_height
+)
+{
+	int x_end;
+	int y_end;
+	
+	DigitalWrite(reset_pin, LOW);                //module reset
+	DelayMs(10);
+	DigitalWrite(reset_pin, HIGH);
+	DelayMs(10);
+	SendCommand(0x3c);
+	SendData(0x80);
 
-  EPD_Update();
+	if (
+	        image_buffer == NULL ||
+	        x < 0 || image_width < 0 ||
+	        y < 0 || image_height < 0
+	) {
+		return;
+	}
+	/* x point must be the multiple of 8 or the last 3 bits will be ignored */
+	x &= 0xF8;
+	image_width &= 0xF8;
+	if (x + image_width >= this->width) {
+		x_end = this->width - 1;
+	} else {
+		x_end = x + image_width - 1;
+	}
+	if (y + image_height >= this->height) {
+		y_end = this->height - 1;
+	} else {
+		y_end = y + image_height - 1;
+	}
+	SetMemoryArea(x, y, x_end, y_end);
+	SetMemoryPointer(x, y);
+	SendCommand(0x24);
+	/* send the image data */
+	for (int j = 0; j < y_end - y + 1; j++) {
+		for (int i = 0; i < (x_end - x + 1) / 8; i++) {
+			SendData(image_buffer[i + j * (image_width / 8)]);
+		}
+	}
+}
+
+/**
+ *  @brief: After this command is transmitted, the chip would enter the
+ *          deep-sleep mode to save power.
+ *          The deep sleep mode would return to standby by hardware reset.
+ *          The only one parameter is a check code, the command would be
+ *          executed if check code = 0xA5.
+ *          You can use Epd::Init() to awaken
+ */
+void DEPG0150BxS810FxX_BW::Sleep()
+{
+	SendCommand(0x10); //enter deep sleep
+	SendData(0x01);
+	DelayMs(200);
+
+	DigitalWrite(reset_pin, LOW);
 }
 
 /** DEPG0150BxS810FxX_BW.cpp END **/
--- src/DEPG0150BxS810FxX_BW.h
+++ src/DEPG0150BxS810FxX_BW.h
@@ -7,28 +7,43 @@
 #define	 MAX_LINE_BYTES   25
 #define  MAX_COLUMN_BYTES 200	
 #define  ALLSCREEN_BYTES  5000
+
 /* Display resolution */
-// #define EPD_WIDTH       
-// #define EPD_HEIGHT      
+#define EPD_WIDTH       200
+#define EPD_HEIGHT      200
+
 
 class DEPG0150BxS810FxX_BW:public EpdBase {
-    public:
-        DEPG0150BxS810FxX_BW(uint8_t _rst, uint8_t _dc, uint8_t _cs,  uint8_t _busy, int8_t _clk=-1, uint8_t _spi_num = 0 ,uint32_t _freq = 2000000): \
-        EpdBase(_rst, _dc, _cs, _busy, _clk, _spi_num, _freq){}
+	public:
+		DEPG0150BxS810FxX_BW(uint8_t _rst, uint8_t _dc, uint8_t _cs,  uint8_t _busy, int8_t _clk=-1, uint8_t _spi_num = 0 ,uint32_t _freq = 2000000): \
+		EpdBase(_rst, _dc, _cs, _busy, _clk, _spi_num, _freq){}
 
-        ~DEPG0150BxS810FxX_BW(){}
+		~DEPG0150BxS810FxX_BW(){}
+		int LDirInit(void);
+		int HDirInit(void);
+		void Clear(void);
+		void Display(const unsigned char* frame_buffer);
+		void DisplayPartBaseImage(const unsigned char* frame_buffer);
+		void DisplayPartBaseWhiteImage(void);
+		void DisplayPart(const unsigned char* frame_buffer);
+		void SetFrameMemory(
+						const unsigned char* image_buffer,
+						int x,
+						int y,
+						int image_width,
+						int image_height
+		);
+		void DisplayFrame(void);
+		void DisplayPartFrame(void);
 
-    public:
-        void EPD_Init(void); //Electronic paper initialization
-        void EPD_Update(void);  
-        void EPD_DeepSleep(void);
-        //Display 
-        void EPD_ALL_image(const unsigned char *datas);
-        void EPD_WhiteScreen_Black(void);
-        void EPD_WhiteScreen_White(void);
+		void Sleep(void);
 
-    private:
-        void EPD_Load_Data(unsigned char data);
+	private:
+		unsigned int spiFlag = 0;
+		unsigned long width = EPD_WIDTH;
+		unsigned long height = EPD_HEIGHT;
+		void SetMemoryArea(int x_start, int y_start, int x_end, int y_end);
+		void SetMemoryPointer(int x, int y);
 
 };
 
