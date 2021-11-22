72c066aa527380b8b7ffdec4653666df920a5548
Quency-D <hj_zzns@163.com>
2021-11-22 15:12:25 +0800

    Add 2.13 partial refresh.
    Add 2.13 partial refresh.

--- examples/DEPG0213Bx800FxX_BW/DEPG0213Bx800FxX_BW.ino
+++ examples/DEPG0213Bx800FxX_BW/DEPG0213Bx800FxX_BW.ino
@@ -4,6 +4,7 @@ Display Resolution 122(H)×250(V)
 */
 #include "DEPG0213Bx800FxX_BW.h"
 #include "picture.h"
+#include "e_ink_display.h"
 
 #define UNDEFINED -1
 #ifdef CubeCell_Board //AB01
@@ -11,11 +12,11 @@ Display Resolution 122(H)×250(V)
 
     DEPG0213Bx800FxX_BW epd213bw(GPIO1, GPIO2, GPIO3, GPIO5, UNDEFINED, SPI_NUM_0, FREQUENCY);//reset_pin, dc_pin, cs_pin, busy_pin, clk_pin, spi_num, frequency
 #elif defined( ESP32 )
-    #define RST_PIN         16
-    #define DC_PIN          22
-    #define CS_PIN          18
-    #define BUSY_PIN        23
-    #define CLK_PIN         5
+    #define RST_PIN         4
+    #define DC_PIN          13
+    #define CS_PIN          15
+    #define BUSY_PIN        17
+    #define CLK_PIN         16
 
     DEPG0213Bx800FxX_BW epd213bw(RST_PIN, DC_PIN, CS_PIN, BUSY_PIN, CLK_PIN);//reset_pin, dc_pin, cs_pin, busy_pin, clk_pin
 #elif defined( ESP8266 )
@@ -33,17 +34,41 @@ Display Resolution 122(H)×250(V)
     #define BUSY_PIN        7
 #endif
 
+
+unsigned char img[1024];
+Paint pt(img, 0, 0);
+unsigned long time_start_ms;
+unsigned long time_now_s;
+#define COLORED     0
+#define UNCOLORED   1
+
+
+
+
 void setup() {
     Serial.begin(115200);
     epd213bw.EPD_Init(); //Electronic paper initialization
     epd213bw.EPD_ALL_image(gImage_213image_bw);	//Refresh the picture in full screen
+    delay(2000);
+    pt.Clear(COLORED);
+    pt.SetWidth(100);
+    pt.SetHeight(124);
+    pt.DrawStringAt(10,11, "2.13!", &Font16, UNCOLORED);
+    epd213bw.SetFrameMemory(pt.GetImage(), 0, 0, pt.GetWidth(), pt.GetHeight());
+    epd213bw.HalLcd_Partial_Update();
+    delay(2000);
 
-    // delay(1000);
-    // epd213bw.EPD_WhiteScreen_Black();
-    // delay(1000);
-    // epd213bw.EPD_WhiteScreen_White();
+    pt.SetWidth(64);
+	pt.SetHeight(64);
+
+	pt.Clear(UNCOLORED);
+	pt.DrawRectangle(0, 0, 40, 50, COLORED);
+	pt.DrawLine(0, 0, 40, 50, COLORED);
+	pt.DrawLine(40, 0, 0, 50, COLORED);
+	epd213bw.SetFrameMemory(pt.GetImage(), 16, 60, pt.GetWidth(), pt.GetHeight());
+    epd213bw.HalLcd_Partial_Update();
+    delay(100);
     epd213bw.EPD_DeepSleep();  //Enter deep sleep
-
 }
 void loop() {
 
